752f9b212c1de99df4110b4de106f790537bdba7
Michel Wehrli <wehrli92@gmail.com>
2021-01-28 21:48:38 +0100

    build process changes

--- tasks/rules/scripts.js
+++ tasks/rules/scripts.js
@@ -1,18 +1,24 @@
 module.exports = (env) => {
-  return [{
+  return [
+    {
       test: /\.ts$/,
       exclude: /node_modules/,
-      use: [{
-          loader: 'babel-loader'
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
         },
         {
           loader: 'ts-loader',
           options: {
             transpileOnly: true, // For hot reloading,
-            experimentalWatchApi: true
-          }
-        }
-      ]
-    }
+            experimentalWatchApi: true,
+          },
+        },
+      ],
+    },
   ]
-};
+}
